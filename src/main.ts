0adfb0cc92fc67dfc8b0991d525a9f42f152b057
yashweblife <yashweblife20@gmail.com>
2023-05-09 20:17:15 -0500

    new class experiments

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,31 @@
+class Matrix{
+    constructor(public size:number=3){
+        
+    }
+}
+class Canvas{
+    public canvas= document.createElement("canvas");
+    public c= this.canvas.getContext('2d');
+    public size=new Matrix(2);
+    constructor(parent:HTMLElement=document.body){}
+}
+
+
+class Group{
+    public values:any[] = []
+    constructor(public name:string="", data:any){
+        this.values.push(data);
+    }
+    public addValue(){}  
+}
 class Classifier{
     constructor(){}
     public gatherData(){}
     public classify(){}
     public train(){}
+}
+
+class Visualizer{
+    public canvas:Canvas = new Canvas();
+    constructor(){}
 }
