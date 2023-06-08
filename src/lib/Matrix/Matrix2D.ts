f5ca1e8aba18664d2451a9fa071c924d1ed2a9bf
yashweblife <yashweblife20@gmail.com>
2023-06-08 00:13:19 -0500

    add functions

--- src/lib/Matrix/Matrix2D.ts
+++ src/lib/Matrix/Matrix2D.ts
@@ -7,11 +7,38 @@ export class Matrix2D {
     }
     public add(m: Matrix2D) {
         if (this.rows !== m.rows || this.cols !== m.cols) return;
-
         for (let i = 0; i < this.rows; i++) {
             for (let j = 0; j < this.rows; j++) {
                 this.components[i][j] += m.components[i][j]
             }
         }
+        return (this)
+    }
+    public sub(m: Matrix2D) {
+        if (this.rows !== m.rows || this.cols !== m.cols) return;
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.rows; j++) {
+                this.components[i][j] -= m.components[i][j]
+            }
+        }
+        return (this)
+    }
+    public mul(m: Matrix2D) {
+        if (this.rows !== m.rows || this.cols !== m.cols) return;
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.rows; j++) {
+                this.components[i][j] *= m.components[i][j]
+            }
+        }
+        return (this)
+    }
+    public div(m: Matrix2D) {
+        if (this.rows !== m.rows || this.cols !== m.cols) return;
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.rows; j++) {
+                this.components[i][j] /= m.components[i][j]
+            }
+        }
+        return (this)
     }
 }
