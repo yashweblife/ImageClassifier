801c69b6956312bdd02cb9838965437dff91e72b
yashweblife <yashweblife20@gmail.com>
2023-06-07 23:44:06 -0500

    add matrix 2d

---
+++ src/lib/Matrix/Matrix2D.ts
@@ -0,0 +1,17 @@
+export class Matrix2D {
+    public components: Float64Array[] = [];
+    constructor(public rows: number, public cols: number) {
+        for (let i = 0; i < rows; i++) {
+            this.components.push(new Float64Array(cols))
+        }
+    }
+    public add(m: Matrix2D) {
+        if (this.rows !== m.rows || this.cols !== m.cols) return;
+
+        for (let i = 0; i < this.rows; i++) {
+            for (let j = 0; j < this.rows; j++) {
+                this.components[i][j] += m.components[i][j]
+            }
+        }
+    }
+}
