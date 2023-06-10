51d0d50f5ba01f5e0798007b44207e3fb6607186
yashweblife <yashweblife20@gmail.com>
2023-06-10 03:56:46 -0500

    add basic functions

--- src/lib/DOM/dommer.ts
+++ src/lib/DOM/dommer.ts
@@ -1,3 +1,18 @@
-export class ${
-    constructor(){}
+export class $ {
+    public dom: HTMLElement;
+    constructor(tag: string) {
+        this.dom = document.createElement(tag)
+    }
+    public addClass(val: string) {
+        this.dom.classList.add(val);
+        return (this)
+    }
+    public removeClass(val: string) {
+        this.dom.classList.remove(val);
+        return (this)
+    }
+    public setText(val: string) {
+        this.dom.innerText = val;
+        return this
+    }
 }
